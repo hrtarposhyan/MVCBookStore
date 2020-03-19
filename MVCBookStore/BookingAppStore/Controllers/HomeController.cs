a715d6c7cfeff177e134f71e96746d0c78b7d050
hrtarposhyan <hrtarposhyan@gmail.com>
2020-03-19 06:03:36 -0700

    BookStore

--- MVCBookStore/BookingAppStore/Controllers/HomeController.cs
+++ MVCBookStore/BookingAppStore/Controllers/HomeController.cs
@@ -9,13 +9,32 @@ namespace BookingAppStore.Controllers
 {
     public class HomeController : Controller
     {
-        BookContext db=new BookContext();
+        BookContext db = new BookContext();
         public ActionResult Index()
         {
             var books = db.Books;
             ViewBag.Books = books;
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Purchese purchese)
+        {
+            purchese.Date = DateTime.Now;
+            // db-um avelacnum enq vaharqi masin
+            db.Purcheses.Add(purchese);
+            // soxranit enq anum bolor popoxutyunnern bd-um
+            db.SaveChanges();
+            return "Շնորհակալություն," + purchese.Person + ",Գնումների համար!!!";
+        }
+
 
         public ActionResult About()
         {
